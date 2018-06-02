ac10f5373253f4e1b2a4a965c4ecf45a94f313d8
Victor Wu <14853635+victorwu95@users.noreply.github.com>
2018-06-02 12:39:41 -0700

    Update CSS styling

--- src/App.js
+++ src/App.js
@@ -26,41 +26,34 @@ class App extends Component {
     return (
       <div className="App">
         <form onSubmit={onFormSubmit} className="Form">
-          <h1>Add Your Club</h1>
+          <h1>Add Club</h1>
           <input type="text" name="clubName" placeholder="Club Name" className="Form-Input"/>
-          <br/>
-          <button className="Form-Button">Add pictures</button>
+          <button className="Form-Button Form-Buttom-Picture">Add pictures</button>
           <input type="text" name="tags" placeholder="Tags" className="Form-Input" />
-          <br/>
           <textarea rows="4" cols="50" placeholder="About"
           className="Form-Input Form-Input-About"></textarea>
-          <br/>
-          <br/>
           <div className="Social-Media">
             <span>Social media:</span>
             <div className="Social-Media-Type">
               <p>Fb</p>
-              <button>+</button>
+              <button className="Form-Button">+</button>
             </div>
             <div className="Social-Media-Type">
               <p>Ins</p>
-              <button>+</button>
+              <button className="Form-Button">+</button>
             </div>
             <div className="Social-Media-Type">
               <p>Twitter</p>
-              <button>+</button>
+              <button className="Form-Button">+</button>
             </div>
           </div>
-          <br/>
           <input type="text" name="meetingLocation" placeholder="Meeting location" className="Form-Input"/>
-          <br/>
           <input type="text" name="meetingTimes" placeholder="Meeting times" className="Form-Input"/>
-          <br/>
           <input type="text" name="website" placeholder="website URL" className="Form-Input"/>
-          <br/>
           <input type="text" name="events" placeholder="Events" className="Form-Input"/>
-          <br/>
-          <button>submit</button>
+          <button>
+            submit
+          </button>
         </form>
       </div>
     );
@@ -68,23 +61,3 @@ class App extends Component {
 }
 
 export default App;
-
-// const app={
-//   title: 'Indecision App',
-//   subtitle: 'This is some info'
-// };
-
-// var template = (
-//   <div>
-//       <h1>{app.title}</h1>
-//       <p>{app.subtitle}</p>
-//       <ol>
-//           <li>Item one</li>
-//           <li>Item two</li>
-//       </ol>
-//   </div>
-// );
-
-
-// const appRoot = document.getElementById('root');
-// ReactDOM.render(template, appRoot);
