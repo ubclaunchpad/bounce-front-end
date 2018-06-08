f57ddd9898b3bdddb987d04127f983b30c636cb5
Bruno Bachmann <bfbachmann@gmail.com>
2018-06-07 21:46:30 -0700

    Add Makefile, minor code cleanup

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
-import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+/* eslint-disable-next-line no-unused-vars */
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
