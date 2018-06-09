07cd5d932c0728c44c1f7b4051d12496fbfc8d07
Victor Wu <14853635+victorwu95@users.noreply.github.com>
2018-06-09 08:54:20 -0700

    Fixed reviews

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import logo from './logo.svg';
 import './App.css';
 import { template } from 'handlebars';
 
@@ -17,21 +16,49 @@ const onFormSubmit = (e) => {
     information.clubName = info;
     e.target.elements.clubName.value = '';
   }
-
+  console.log(e);
   console.log(information);
 };
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      clubName: '',
+      about: '',
+    };
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleInputChange = this.handleInputChange.bind(this);
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+  }
+
+  handleInputChange(event) {
+    const target = event.target;
+    const name = target.name;
+    const value = target.value;
+
+    this.setState({
+      [name]: value
+    });
+  }
+
   render() {
     return (
       <div className="App">
         <form onSubmit={onFormSubmit} className="Form">
           <h1>Add Club</h1>
-          <input type="text" name="clubName" placeholder="Club Name" className="Form-Input"/>
+          <input type="text" name="clubName" placeholder="Club Name" className="Form-Input"
+          value={this.state.clubName}
+          onChange={this.handleInputChange}/>
           <button className="Form-Button Form-Buttom-Picture">Add pictures</button>
           <input type="text" name="tags" placeholder="Tags" className="Form-Input" />
-          <textarea rows="4" cols="50" placeholder="About"
-          className="Form-Input Form-Input-About"></textarea>
+          <textarea rows="4" cols="50" name="about" placeholder="About"
+          className="Form-Input Form-Input-About"
+          value={this.state.about} onChange={this.handleInputChange}></textarea>
           <div className="Social-Media">
             <span>Social media:</span>
             <div className="Social-Media-Type">
