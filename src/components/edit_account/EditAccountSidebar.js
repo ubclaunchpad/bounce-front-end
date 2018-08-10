3c1bfd836e15ac3cbec995f416b254a8a80d8973
Victor Wu <14853635+victorwu95@users.noreply.github.com>
2018-08-09 22:05:29 -0700

    Initial Edit Account Page

---
+++ src/components/edit_account/EditAccountSidebar.js
@@ -0,0 +1,57 @@
+/* eslint-disable no-unused-vars */
+import React, { Component } from 'react';
+import '../../css/EditPage.css'
+/* eslint-enable no-unused-vars */
+
+class EditAccountSidebar extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            currentSelected: '',
+        };
+    }
+
+    handleOnclick() {
+        this.props.onSelectSidebar
+        this.props.sidebarSelectionOptions;
+    }
+
+    render() {
+        return (
+            <div className="sidebar">
+                <h3 className="sidebarLabel">Profile Settings</h3>
+                <div className="selectionContainer" 
+                    onClick={() => this.props.onSelectSidebar("Profile")}>
+                    <span className="selectionWord">
+                        Profile
+                    </span>
+                </div>
+                <div className="selectionContainer">
+                    <span className="selectionWord">
+                        Clubs Affiliation
+                    </span>
+                </div>
+                <div className="selectionContainer">
+                    <span className="selectionWord">
+                        Interest
+                    </span>
+                </div>
+                <hr/>
+                <h3 className="sidebarLabel">Account Setting</h3>
+                <div className="selectionContainer"
+                    onClick={() => this.props.onSelectSidebar("Account")}>
+                    <span className="selectionWord">
+                        Account
+                    </span>
+                </div>
+                <div className="selectionContainer">
+                    <span className="selectionWord">
+                        Privacy
+                    </span>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default EditAccountSidebar;
