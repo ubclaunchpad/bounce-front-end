30893aedb1bb770b700423c6fb6d54f8a219bf50
RachitMalik12 <rachitmalik1947@gmail.com>
2019-02-02 16:22:13 -0800

    Fixed indentation

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,17 +1,17 @@
 import {ADD_USER} from '../constants/action-types';
 
 const initialState =  {
-  user:{username:'',
+    user:{username:'',
         fullName:'',
         email : ''}
 };
 
 function rootReducer(state = initialState, action){
     if (action.type == ADD_USER){
-      return Object.assign({}, state, {
-      user: action.payload
- });
-      }
+        return Object.assign({}, state, {
+            user: action.payload
+        });
+    }
     return state;
 }
 
