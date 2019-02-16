d1bb2fc09c7fb4bbaf3dfac1bd6bb4d3e919c44b
kevintabouguia <kevinyok@alumni.ubc.ca>
2019-02-16 10:39:47 -0800

    Started setting up front-end tests

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -3,7 +3,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import App from './App';
+import App from '../components/App';
 import BounceClient from '../api';
 /* eslint-enable no-unused-vars */
 
